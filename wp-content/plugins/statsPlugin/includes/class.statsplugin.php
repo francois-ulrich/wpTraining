71a63f0b71627d06d6912e13b00185c2466fdb3f
François Ulrich <fulrich3@gmail.com>
2024-12-20 11:27:51 +0100

    feat: page statistics plugin

--- wp-content/plugins/statsPlugin/includes/class.statsplugin.php
+++ wp-content/plugins/statsPlugin/includes/class.statsplugin.php
@@ -17,15 +17,35 @@ class StatsPlugin{
   }
 
   function render($content){
-    $data = [
-        'title' => "Page stats",
-    ];
-
     $templateLoader = new FilesystemLoader(STATSPLUGIN_TEMPLATES_DIR);
     $twig = new Environment($templateLoader);
 
+    $display = [
+      "wordCount" => boolval(get_option("sp_wordcount", "0") == "1"),
+      "characterCount" => boolval(get_option("sp_charactercount", "0") == "1"),
+      "readTime" => boolval(get_option("sp_readtime", "0") == "1"),
+    ];
+
+
+    $rawText = strip_tags($content);
+
+    $wordCount = str_word_count($rawText);
+    $characterCount = strlen(str_replace(' ', '', $rawText));
+    $readTimeInMinutes = round($wordCount/255);
+
+    $data = [
+        'headlineText' => get_option("sp_headlinetext", "Page stats"),
+        "display" => $display,
+        'wordCount' => $wordCount,
+        'characterCount' => $characterCount,
+        'readTimeInMinutes' => $readTimeInMinutes,
+    ];
+
     $renderedTemplate = $twig->render('stats-block.html.twig', $data);
 
-    return $content . $renderedTemplate;
+    if(get_option("sp_displaylocation", "start") == "end")
+      return $content . $renderedTemplate;
+
+    return $renderedTemplate . $content;
   }
 }

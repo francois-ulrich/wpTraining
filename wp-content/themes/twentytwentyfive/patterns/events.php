e8cd6c6affc0fc3776262651f49c092aab16f225
François Ulrich <fulrich3@gmail.com>
2024-12-26 14:14:07 +0100

    added event post type, added twentytwentyfive theme to gitignore whitelist for edition

---
+++ wp-content/themes/twentytwentyfive/patterns/events.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Events
+ * Slug: twentytwentyfive/events
+ * Categories: about
+ * Description: A section for displaying events
+ * Keywords: events
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Twenty Twenty-Five 1.0
+ */
+
+ 
+?>
+
+<p>hyyyyyyyyyey</p>

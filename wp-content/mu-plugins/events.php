0d70f54089992cea83e51475f345e7fed3fd3da1
François Ulrich <fulrich3@gmail.com>
2024-12-26 14:10:22 +0100

    added event post type, added twentytwentyfive theme to gitignore whitelist for edition

---
+++ wp-content/mu-plugins/events.php
@@ -0,0 +1,19 @@
+<?php
+
+
+defined('ABSPATH') or die('Direct access not allowed.');
+
+add_action("init", "university_post_types");
+
+function university_post_types() {
+	register_post_type("event", array(
+		"public" => true,
+		"labels" => array(
+			"name"=>"Events",
+			"add_new_item"=>"Add new event",
+			"edit_item"=>"Edit Event",
+			"all_items"=>"All events",
+		),
+		"menu_icon" => "dashicons-calendar-alt",
+	));
+}
